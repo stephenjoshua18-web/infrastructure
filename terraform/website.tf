09e41851ca2aaf54a3a150c47bc2e1df9efeee3e
Joshua Okorie <okorie@glade.ng>
2023-03-22 18:05:37 +0000

    Update terraform/website.tf

--- terraform/website.tf
+++ terraform/website.tf
@@ -1,7 +1,14 @@
-resource "aws_route53_record" "pay_external_dns" {
-  zone_id = aws_route53_zone.gladeng_zone.zone_id
-  name    = "pay-external-prod-aws.glade.ng"
+resource "aws_route53_record" "blog_external_dns" {
+  zone_id = aws_route53_zone.gladefinance_zone.zone_id
+  name    = "blog.gladefinance.co"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.pay_service.public_ip]
+  records = [var.website_ip]
+}
+resource "aws_route53_record" "blog_backend_external_dns" {
+  zone_id = aws_route53_zone.gladefinance_zone.zone_id
+  name    = "blog-backend.gladefinance.co"
+  type    = "A"
+  ttl     = 300
+  records = [var.website_ip]
 }
