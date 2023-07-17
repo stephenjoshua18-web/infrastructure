1d09aaf93b2ad790a28d4b62796275af2062faef
Joshua Okorie <okorie@glade.ng>
2023-07-17 16:53:21 +0000

    updated urls for webhook, core and api

--- terraform/service_external_api.tf
+++ terraform/service_external_api.tf
@@ -36,6 +36,14 @@ resource "aws_route53_record" "api_external_dns" {
   records = [aws_instance.external_api_service.public_ip]
 }
 
+resource "aws_route53_record" "api_25_external_dns" {
+  zone_id = aws_route53_zone.gladeng_zone.zone_id
+  name    = "api-prod.glade.ng"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.external_api_service.public_ip]
+}
+
 
 resource "aws_route53_record" "external_api_2_internal_dns" {
   zone_id = aws_route53_zone.gladefinance_zone.zone_id
@@ -44,6 +52,15 @@ resource "aws_route53_record" "external_api_2_internal_dns" {
   ttl     = 300
   records = [aws_instance.external_api_service.private_ip]
 }
+
+resource "aws_route53_record" "external_api_26_internal_dns" {
+  zone_id = aws_route53_zone.gladefinance_zone.zone_id
+  name    = "api-prod.gladefinance.co"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.external_api_service.private_ip]
+}
+
 
 resource "aws_route53_record" "external_api_211_external_dns" {
   zone_id = aws_route53_zone.gladefinance_zone.zone_id
